c87b8c724cb30d7fdde8063ca234373e338b8e5f
Mihai <mihainstein@gmail.com>
2021-07-17 14:22:08 +0100

    v.0.2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '646b65d1aeae6b121eceaaa9378eff30064a7219',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '646b65d1aeae6b121eceaaa9378eff30064a7219',
+    ),
+  ),
+);
