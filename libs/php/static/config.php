0c2548da3471ce364760cf234eda58c1ab76c17b
Mihai <mihainstein@gmail.com>
2021-07-17 14:35:53 +0100

    v.0.3

--- libs/php/static/config.php
+++ libs/php/static/config.php
@@ -2,6 +2,6 @@
 $cd_host = "eu-cdbr-west-01.cleardb.com";
 $cd_user = "b3047326063983";
 $cd_password = "186373b1";
-$cd_dbname = "companydirectory";
+$cd_dbname = "heroku_8a9144044437894";
 $cd_port =  3306;
 $cd_socket = "";
