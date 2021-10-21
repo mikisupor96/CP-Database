2acbcee6bdd95d376fe419a382777177f7b36049
Mihai Gabriel Pascaru <30627752+mikisupor96@users.noreply.github.com>
2021-10-21 19:59:04 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,21 +1,9 @@
-# Company CMS
-*For all your data entry needs*
+1. Implement results for dep change and location change [COMPLETE]
+2. Implement results for searches on each of the category [COMPLETE]
+3. Change modal based on category change [COMPLETE]
+4. Implement Adding to each of the categories [COMPLETE]
+5. Implement Editing for each of the category [COMPLETE]
+6. Implement Deleting for each of the category [COMPLETE]
 
-## What is this project about?
-
-This project was designed to replicate all the CRUD functionalities found in any data entry application i.e GET, POST, PUT, DELETE. As a user you are able to look for an users information, add a new user, edit a current user and delete them aswell. The information in the tables is:
-* First name
-* Last name
-* email
-* department 
-* location
-
-## How does it work?
-
-When a user attempt to make a search by pressing the search button the app will check if the information is correct by checking the information the back end has sent back if it is correct it will display the user(s) if it is wrong it will display an error message, this app is a fullstack PHP application.
-
-## Technical Information
-
-The app uses HTML, Javascript, Jquery and Bootstrap on the front end and PHP and MySql on the back end. When the user first loads the page the forms on the page get loaded with the current departments in the database using an ajax call through a php routine. After this the when a user makes a search by selecting the different categories the app checks the data agains the database again using a php routine and returns either an error or the information. 
-
-> Hope you like my app and if you have any questions/suggestions feel free to contact me at mihainstein@gmail.com  :smiley:
+- Put a small modal asking for confirmation on deletion [COMPLETE]
+- Do not allow users departments if there are personell inside it [COMPLETE]
--- script.js
+++ script.js
@@ -1,256 +1,87 @@
 import getAll from "./libs/js/getAll.js";
-import searchByFirstName from "./libs/js/searchByFirstName.js";
-import searchByLastName from "./libs/js/searchByLastName.js";
-import searchByEmail from "./libs/js/searchByEmail.js";
-import searchByDepartment from "./libs/js/searchByDepartment.js";
-import searchByLocation from "./libs/js/searchByLocation.js";
+import getPersonnel from "./libs/js/getPersonnel.js";
 import getAllDepartments from "./libs/js/getAllDepartments.js";
+import getDepartment from "./libs/js/getDepartment.js";
+import getAllLocations from "./libs/js/getAllLocations.js";
+import getLocation from "./libs/js/getLocation.js";
 import addNewUser from "./libs/js/addNewUser.js";
-import deleteUser from "./libs/js/deleteUser.js";
-import editUser from "./libs/js/editUser.js";
+import addNewDepartment from "./libs/js/addNewDepartment.js";
+import addNewLocation from "./libs/js/addNewLocation.js";
+import populateLocations from "./libs/js/populateLocations.js";
+import populateDepartments from "./libs/js/populateDepartments.js";
+import errorResult from "./libs/js/Dom/errorResult.js";
 
 $(document).ready(() => {
-  // Execute info modal
-  $("#infoModal").modal({
-    keyboard: true,
-  });
-
-  // toggle on off the department option
-  $("#newDepBtn").click(function () {
-    const btnHtml = $(this).html().trim();
-    if (btnHtml === "+") {
-      $(this).html("-");
-    } else {
-      $(this).html("+");
-    }
-    $("#departmentSelect").toggleClass("collapse");
-    $("#addLocation").toggleClass("collapse");
-  });
-
-  // Prevent form resubmit //
   $("form").submit(() => {
     return false;
   });
 
-  // Render options in departments
-  (async () => {
-    const result = await getAllDepartments();
-    result["data"].forEach((el) => {
-      $("#departmentSelect").append($("<option />").val(el).text(el));
-    });
-  })();
+  populateDepartments();
+
+  getAll();
 });
 
-function createFeedback(value, inputDom, valid, invalid) {
-  // feedback for server values
-
-  if (value) {
-    inputDom
-      .removeClass("is-invalid")
-      .addClass("is-valid")
-      .after(`<div class='feedback valid-feedback'>${valid}</div>`);
-  } else {
-    inputDom
-      .removeClass("is-valid")
-      .addClass("is-invalid")
-      .after(`<div class='feedback invalid-feedback'>${invalid}</div>`);
-  }
-}
-
-function validateUserData(firstName, lastName, email, department, locationID) {
-  (async () => {
-    const validateData = await addNewUser(
-      firstName,
-      lastName,
-      email,
-      department,
-      locationID
-    );
-
-    const { firstNameExist, lastNameExist, emailExist } = validateData;
-
-    $(".feedback").remove();
-
-    createFeedback(
-      firstNameExist,
-      $("#firstName"),
-      "Valid first name!",
-      "First name already taken!"
-    );
-
-    createFeedback(
-      lastNameExist,
-      $("#lastName"),
-      "Valid last name!",
-      "Last name already taken!"
-    );
-
-    createFeedback(
-      emailExist,
-      $("#email"),
-      "Valid email!",
-      "Email already taken!"
-    );
-  })();
-}
-
-// ----------------------------TEST----------------------------
-// (() => {})();
-// ----------------------------TEST----------------------------
-
-// *Get data from add new user form
-$(".createUser").submit(() => {
-  $(".alert").remove();
+$(".createUser").submit(function () {
   const firstName = $("#firstName").val();
   const lastName = $("#lastName").val();
   const email = $("#email").val();
-  const locationID = $("#addLocation").val();
-  let department = $("#departmentSelect").val();
+  const department = $(".department-select").val();
 
-  const depCondition = $("#newDepBtn").attr("aria-expanded");
-
-  if (depCondition === "true") {
-    department = $("#addDepartment").val();
-  }
-
-  validateUserData(firstName, lastName, email, department, locationID);
+  addNewUser(firstName, lastName, email, department);
 });
 
-// Action on use search //
+$(".createDepartment").submit(function () {
+  const department = $("#departmentName").val();
+  const location = $("#locationOfDepartment").val();
+
+  addNewDepartment(department, location);
+});
+
+$(".createLocation").submit(function () {
+  const location = $("#locationName").val();
+
+  addNewLocation(location);
+});
+
 $("#searchButton").click(() => {
   const select = $("#searchOption").val();
   const value = $("#searchBox").val();
   const result = $(".result");
 
-  // Clear everything on change
   $("#searchOption").change(() => {
     result.empty();
   });
 
-  // Clear previous result
   result.empty();
 
-  // Reset value of search bar
   $("#searchBox").val("");
 
-  // If the user leaves blank it means that he wants to see all users from specific select
-  if (select === "Category" && !value) {
-    render(getAll);
-  } else if (select === "firstName") {
-    render(searchByFirstName, value);
-  } else if (select === "lastName") {
-    render(searchByLastName, value);
-  } else if (select === "email") {
-    render(searchByEmail, value);
-  } else if (select === "department") {
-    render(searchByDepartment, value);
-  } else if (select === "location") {
-    render(searchByLocation, value);
+  if (select === "personnel") {
+    value !== "" ? getPersonnel(value) : getAll();
+  } else if (select === "departments") {
+    value !== "" ? getDepartment(value) : getAllDepartments();
+  } else if (select === "locations") {
+    value !== "" ? getLocation(value) : getAllLocations();
   } else {
-    errorHtml("Please select a search parameter!");
+    errorResult("Please select a search parameter!");
   }
 });
 
-// Render html //
-const errorHtml = (message) => {
-  $(".result").html(`
-  <div class="alert alert-danger" role="alert">
-    ${message}
-  </div>
-  `);
-};
-
-const generateHtml = (firstName, lastName, email, department, location) => {
-  $(".result").append(`
-    <div class="col">
-      <div class="card">
-        <div class="card-body ">
-          <h5 class="card-title"><span>${firstName}</span> <span>${lastName}</span></h5>
-          <p class="card-text">Location: <span>${location}</span></p>
-          <p class="card-text">Department: <span>${department}</span></p>
-          <p class="card-text d-inline-flex">Email: <span>${email}</span></p>
-          <button class="btn btn-danger btn-sm float-right removeUser ">-</button>
-          <button class="btn btn-success btn-sm float-right editUserButton mr-1 " 
-                  data-toggle="modal"
-                  data-target="#editUserModal">
-                  ...
-          </button>
-        </div>
-      </div>
-    </div>
-    `);
-};
-
-// Functionality //
-const render = async (searchFunc, searchParam = null) => {
-  try {
-    const result = await searchFunc(searchParam);
-    result.forEach((el) => {
-      const { firstName, lastName, email, department, location } = el;
-      generateHtml(firstName, lastName, email, department, location);
-    });
-  } catch (e) {
-    errorHtml(`Could not find ${searchParam}.`);
+$("#searchOption").change(function () {
+  const select = $(this).val();
+  if (select === "personnel") {
+    populateDepartments();
+    $("#addButton").attr("data-target", "#addUserModal");
+    $(".result").empty();
+    getAll();
+  } else if (select === "departments") {
+    populateLocations();
+    $("#addButton").attr("data-target", "#addDepartmentModal");
+    $(".result").empty();
+    getAllDepartments();
+  } else if (select === "locations") {
+    $("#addButton").attr("data-target", "#addLocationModal");
+    $(".result").empty();
+    getAllLocations();
   }
-
-  $(".removeUser").on("click", function () {
-    $(this).parent().parent().parent().remove();
-    let result = [];
-    $(this)
-      .parent()
-      .find("span")
-      .each(function () {
-        result.push($(this).html());
-      });
-    const firstName = result[0];
-    deleteUser(firstName);
-  });
-
-  $(".editUserButton").on("click", function () {
-    let result = [];
-    $(this)
-      .parent()
-      .find("span")
-      .each(function () {
-        result.push($(this).html());
-      });
-    const firstName = result[0];
-    const lastName = result[1];
-    const email = result[4];
-
-    const firstNameDom = $("#editUserModal").find("#editFirstName");
-    const lastNameDom = $("#editUserModal").find("#editLastName");
-    const emailDom = $("#editUserModal").find("#editEmail");
-
-    // set values to current values
-    firstNameDom.attr("value", firstName);
-    lastNameDom.attr("value", lastName);
-    emailDom.attr("value", email);
-
-    $(".editUser").submit(() => {
-      const newFirstName = firstNameDom.val();
-      const newLastName = lastNameDom.val();
-      const newEmail = emailDom.val();
-
-      editUser(firstName, lastName, email, newFirstName, newLastName, newEmail);
-
-      $("#editFirstName")
-        .addClass("is-valid")
-        .after(
-          `<div class='feedback valid-feedback'>First name updated successfully!</div>`
-        );
-
-      $("#editLastName")
-        .addClass("is-valid")
-        .after(
-          `<div class='feedback valid-feedback'>Last name updated successfully!</div>`
-        );
-
-      $("#editEmail")
-        .addClass("is-valid")
-        .after(
-          `<div class='feedback valid-feedback'>Email updated successfully!</div>`
-        );
-    });
-  });
-};
+});
