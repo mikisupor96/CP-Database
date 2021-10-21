5b9d19b1d762dc5991c325d1bb5621362e2c46d5
Mihai Gabriel Pascaru <30627752+mikisupor96@users.noreply.github.com>
2021-10-21 17:27:56 +0100

    Delete config.php

--- libs/php/static/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$cd_host = "eu-cdbr-west-01.cleardb.com";
-$cd_user = "b3047326063983";
-$cd_password = "186373b1";
-$cd_dbname = "heroku_8a9144044437894";
-$cd_port =  3306;
-$cd_socket = "";
