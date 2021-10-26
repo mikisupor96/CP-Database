be1794238f7346b8b22472a1d5d7649c26d5d2d6
Mihai Gabriel Pascaru <30627752+mikisupor96@users.noreply.github.com>
2021-10-26 14:51:12 +0100

    Update config.php

--- libs/php/static/config.php
+++ libs/php/static/config.php
@@ -1,7 +1,7 @@
 <?php
-	$cd_host = "localhost";
-	$cd_user = "root";
-	$cd_password = "Intelcore3.";
-	$cd_dbname = "companydirectory";
+	$cd_host = "eu-cdbr-west-01.cleardb.com";
+	$cd_user = "b3047326063983";
+	$cd_password = "186373b1";
+	$cd_dbname = "heroku_8a9144044437894";
 	$cd_port =  3306;
 	$cd_socket = "";
