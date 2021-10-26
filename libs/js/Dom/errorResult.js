9918ad80f18460ab2ea775685fa3f97e5d853d76
Mihai Gabriel Pascaru <30627752+mikisupor96@users.noreply.github.com>
2021-10-26 14:46:13 +0100

    Delete libs/js directory

--- libs/js/Dom/errorResult.js
+++
@@ -1,9 +0,0 @@
-const errorResult = (message) => {
-  $(".result").html(`
-    <div class="alert alert-danger" role="alert">
-      ${message}
-    </div>
-    `);
-};
-
-export default errorResult;
